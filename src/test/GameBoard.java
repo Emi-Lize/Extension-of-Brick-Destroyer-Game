ec015e6517d40188f70d29a345397d7d973ffb83
Emi-Lize <hcyla2@nottingham.edu.my>
2021-11-17 16:30:21 +0800

    Created class GameDesign
    Moved code related to the design of the game from GameBoard to GameDesign

--- src/test/GameBoard.java
+++ src/test/GameBoard.java
@@ -20,23 +20,18 @@ package test;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.font.FontRenderContext;
 
 /**
  * This represents the design of the game and the mechanics of the game
+ * Change:
+ * <ul>
+ *     <li>Moved design variables and methods to GameDesign</li>
+ * </ul>
  */
 public class GameBoard extends JComponent implements KeyListener,MouseListener,MouseMotionListener {
-    private static final String CONTINUE = "Continue";
-    private static final String RESTART = "Restart";
-    private static final String EXIT = "Exit";
-    private static final String PAUSE = "Pause Menu";
-    private static final int TEXT_SIZE = 30;
-    private static final Color MENU_COLOR = new Color(0,255,0);
 
-    private static final int DEF_WIDTH = 600;
-    private static final int DEF_HEIGHT = 450;
-
-    private static final Color BG_COLOR = Color.WHITE;
+    public static final int DEF_WIDTH = 600;
+    public static final int DEF_HEIGHT = 450;
 
     private Timer gameTimer;
     private Wall wall;
@@ -44,7 +39,6 @@ public class GameBoard extends JComponent implements KeyListener,MouseListener,M
 
     private boolean showPauseMenu;
 
-    private Font menuFont;
     private Rectangle continueButtonRect;
     private Rectangle exitButtonRect;
     private Rectangle restartButtonRect;
@@ -52,15 +46,17 @@ public class GameBoard extends JComponent implements KeyListener,MouseListener,M
 
     private DebugConsole debugConsole;
     private GameSystem gameSystem;
+    private GameDesign gameDesign;
 
     /**
-     * This represents the design of the game and how the game operates
+     * This represents the layout of the game and how the game operates
      * Change:
      * <ul>
      *     <li>Created an object of class GameSystem</li>
      *     <li>Edited method calls for methods which have been moved from Wall to GameSystem</li>
      *     <li>Added gameSystem object as an argument for DebugConsole constructor</li>
      *     <li>Moved gameTimer to runGame</li>
+     *     <li>Created an object of class GameDesign</li>
      * </ul>
      * @param owner The window the game board is in
      */
@@ -70,12 +66,11 @@ public class GameBoard extends JComponent implements KeyListener,MouseListener,M
         strLen = 0;
         showPauseMenu = false;
 
-        menuFont = new Font("Monospaced",Font.PLAIN,TEXT_SIZE);
-
-        this.initialize();
         message = "";
         wall = new Wall(new Rectangle(0,0,DEF_WIDTH,DEF_HEIGHT),30,3,6/2); //create levels
         gameSystem = new GameSystem(new Rectangle(0,0,DEF_WIDTH,DEF_HEIGHT), new Point(300,430), wall);
+        gameDesign = new GameDesign(this);
+        initialize();
         debugConsole = new DebugConsole(owner,wall,this, gameSystem); //create debug console
 
         //initialize the first level
@@ -133,11 +128,12 @@ public class GameBoard extends JComponent implements KeyListener,MouseListener,M
 
     /**
      * Sets up the properties of the game board
+     * Change:
+     * <ul>
+     *     <li>Moved design code to GameDesign</li>
+     * </ul>
      */
     private void initialize(){
-        this.setPreferredSize(new Dimension(DEF_WIDTH,DEF_HEIGHT)); //size of gameboard
-        this.setFocusable(true); //can be focused on
-        this.requestFocusInWindow(); //focus on this window after debug console closed
         this.addKeyListener(this); //WindowListener in GameBoard will watch for keyboard and mouse
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
@@ -149,181 +145,50 @@ public class GameBoard extends JComponent implements KeyListener,MouseListener,M
      * <ul>
      *     <li>Changed wall.ball to gameSystem.ball</li>
      *     <li>Changed wall.player to gameSystem.player</li>
+     *     <li>Call methods from GameDesign</li>
+     *     <li>Used getters from GameDesign to get the pause menu buttons</li>
      * </ul>
      * @param g An object which draws the components
      */
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g; //allow it to draw objects
 
-        clear(g2d); //set background
+        gameDesign.clear(g2d); //set background
 
         g2d.setColor(Color.BLUE);
         g2d.drawString(message,250,225); //print blue message
 
-        drawBall(gameSystem.ball,g2d); //draw ball
+        gameDesign.drawBall(gameSystem.ball,g2d); //draw ball
 
         for(Brick b : wall.bricks) //create bricks
             if(!b.isBroken())
-                drawBrick(b,g2d);
+                gameDesign.drawBrick(b,g2d);
 
-        drawPlayer(gameSystem.player,g2d); //draw player
+        gameDesign.drawPlayer(gameSystem.player,g2d); //draw player
 
         if(showPauseMenu)
-            drawMenu(g2d); //pause menu
+            gameDesign.drawMenu(g2d); //pause menu
+            continueButtonRect=gameDesign.getContinueButtonRect();
+            exitButtonRect=gameDesign.getExitButtonRect();
+            restartButtonRect= gameDesign.getRestartButtonRect();
 
         Toolkit.getDefaultToolkit().sync();
     }
 
     /**
-     * Sets the background of the game to white
-     * @param g2d An object which draws 2D components
+     * New Method - Gets the length of the string
+     * @return The integer value of the length of the string
      */
-    private void clear(Graphics2D g2d){
-        Color tmp = g2d.getColor();
-        g2d.setColor(BG_COLOR); //set background to white
-        g2d.fillRect(0,0,getWidth(),getHeight());
-        g2d.setColor(tmp); //get back default colour
+    public int getStrLen() {
+        return strLen;
     }
 
     /**
-     * Draws the brick
-     * @param brick The brick object
-     * @param g2d An object which draws 2D components
+     * New Method - Sets the length of the string
+     * @param strLen An integer value
      */
-    private void drawBrick(Brick brick,Graphics2D g2d){
-        Color tmp = g2d.getColor();
-
-        g2d.setColor(brick.getInnerColor());
-        g2d.fill(brick.getBrick()); //fill brick colour
-
-        g2d.setColor(brick.getBorderColor());
-        g2d.draw(brick.getBrick()); //draw brick outline
-
-        g2d.setColor(tmp); //get back default color
-    }
-
-    /**
-     * Draws the ball
-     * @param ball The ball object
-     * @param g2d An object which draws 2D components
-     */
-    private void drawBall(Ball ball,Graphics2D g2d){
-        Color tmp = g2d.getColor();
-
-        Shape s = ball.getBallFace(); //shape of ball
-
-        g2d.setColor(ball.getInnerColor());
-        g2d.fill(s); //fill ball colour
-
-        g2d.setColor(ball.getBorderColor());
-        g2d.draw(s); //draw ball outline
-
-        g2d.setColor(tmp);
-    }
-
-    /**
-     * Draws the player's rectangle
-     * @param p The player object
-     * @param g2d An object which draws 2D components
-     */
-    private void drawPlayer(Player p,Graphics2D g2d){
-        Color tmp = g2d.getColor();
-
-        Shape s = p.getPlayerFace(); //shape of player
-        g2d.setColor(Player.INNER_COLOR);
-        g2d.fill(s); //fill player colour
-
-        g2d.setColor(Player.BORDER_COLOR);
-        g2d.draw(s); //draw player outline
-
-        g2d.setColor(tmp);
-    }
-
-    /**
-     * Sets up a tint and draws the pause menu
-     * @param g2d An object which draws 2D components
-     */
-    private void drawMenu(Graphics2D g2d){
-        obscureGameBoard(g2d);
-        drawPauseMenu(g2d);
-    }
-
-    /**
-     * Applies a tint over the game board when the pause menu is in effect
-     * @param g2d An object which draws 2D components
-     */
-    private void obscureGameBoard(Graphics2D g2d){
-        Composite tmp = g2d.getComposite();
-        Color tmpColor = g2d.getColor();
-
-        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.55f); //darkened effect during pause menu
-        g2d.setComposite(ac);
-
-        g2d.setColor(Color.BLACK); //colour of effect
-        g2d.fillRect(0,0,DEF_WIDTH,DEF_HEIGHT);
-
-        g2d.setComposite(tmp);
-        g2d.setColor(tmpColor);
-    }
-
-    /**
-     * Draws the pause menu
-     * Change:
-     * <ul>
-     *     <li>Moved code to create the button to drawPauseMenuButton</li>
-     * </ul>
-     * @param g2d An object which draws 2D components
-     */
-    private void drawPauseMenu(Graphics2D g2d){
-        Font tmpFont = g2d.getFont();
-        Color tmpColor = g2d.getColor();
-
-        g2d.setFont(menuFont);
-        g2d.setColor(MENU_COLOR); //set font colour
-
-        if(strLen == 0){
-            FontRenderContext frc = g2d.getFontRenderContext();
-            strLen = menuFont.getStringBounds(PAUSE,frc).getBounds().width; //gets width of word PAUSE with that font
-        }
-
-        int x = (this.getWidth() - strLen) / 2; //center title
-        int y = this.getHeight() / 10;
-
-        g2d.drawString(PAUSE,x,y); //put PAUSE MENU
-
-        y = this.getHeight() / 4; //bottom of first quarter
-
-        continueButtonRect=drawPauseMenuButton(g2d, y, continueButtonRect, CONTINUE);
-
-        y *= 2; //second quarter
-
-        restartButtonRect=drawPauseMenuButton(g2d, y, restartButtonRect, RESTART);
-
-        y *= 3.0/2; //third quarter
-
-        exitButtonRect=drawPauseMenuButton(g2d, y, exitButtonRect, EXIT);
-
-        g2d.setFont(tmpFont);
-        g2d.setColor(tmpColor);
-    }
-
-    /**
-     * New Method - Moved code to create the buttons in pause menu from drawPauseMenu
-     * @param g2d An object which draws 2D components
-     * @param y The y-coordinate of the top left of the text
-     * @param rectangle The rectangle of the button
-     * @param text The string in the button
-     * @return The rectangle of the button
-     */
-    private Rectangle drawPauseMenuButton(Graphics2D g2d, int y, Rectangle rectangle, String text){
-        int x = this.getWidth() / 8; //margin for 3 options
-        if (rectangle==null){
-            FontRenderContext frc = g2d.getFontRenderContext();
-            rectangle = menuFont.getStringBounds(CONTINUE,frc).getBounds(); //get width of word CONTINUE
-            rectangle.setLocation(x,y-rectangle.height); //put the button - button sits on the line of quarter
-        }
-        g2d.drawString(text,x,y); //put text
-        return rectangle;
+    public void setStrLen(int strLen) {
+        this.strLen = strLen;
     }
 
     @Override
---
+++ src/test/GameDesign.java
@@ -0,0 +1,228 @@
+package test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+
+/**
+ * New Class - Moved design related variables and methods from GameBoard to this class
+ */
+public class GameDesign extends JComponent{
+
+    private static final String CONTINUE = "Continue";
+    private static final String RESTART = "Restart";
+    private static final String EXIT = "Exit";
+    private static final String PAUSE = "Pause Menu";
+    private static final int TEXT_SIZE = 30;
+    private static final Color MENU_COLOR = new Color(0,255,0);
+    private static final Color BG_COLOR = Color.WHITE;
+
+    private Font menuFont;
+    private Rectangle continueButtonRect;
+    private Rectangle exitButtonRect;
+    private Rectangle restartButtonRect;
+
+    private GameBoard gameBoard;
+
+    /**
+     * This represents the design of the game
+     * @param gameBoard The gameboard object
+     */
+    public GameDesign(GameBoard gameBoard) {
+        menuFont = new Font("Monospaced",Font.PLAIN,TEXT_SIZE);
+
+        this.gameBoard=gameBoard;
+        initialize();
+    }
+
+    /**
+     * Sets up the properties of the game board
+     */
+    private void initialize(){
+        gameBoard.setPreferredSize(new Dimension(gameBoard.DEF_WIDTH,gameBoard.DEF_HEIGHT)); //size of gameboard
+        gameBoard.setFocusable(true); //can be focused on
+        gameBoard.requestFocusInWindow(); //focus on this window after debug console closed
+    }
+
+    /**
+     * Sets the background of the game to white
+     * @param g2d An object which draws 2D components
+     */
+    public void clear(Graphics2D g2d){
+        Color tmp = g2d.getColor();
+        g2d.setColor(BG_COLOR); //set background to white
+        g2d.fillRect(0,0,getWidth(),getHeight());
+        g2d.setColor(tmp); //get back default colour
+    }
+
+    /**
+     * Draws the brick
+     * @param brick The brick object
+     * @param g2d An object which draws 2D components
+     */
+    public void drawBrick(Brick brick,Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        g2d.setColor(brick.getInnerColor());
+        g2d.fill(brick.getBrick()); //fill brick colour
+
+        g2d.setColor(brick.getBorderColor());
+        g2d.draw(brick.getBrick()); //draw brick outline
+
+        g2d.setColor(tmp); //get back default color
+    }
+
+    /**
+     * Draws the ball
+     * @param ball The ball object
+     * @param g2d An object which draws 2D components
+     */
+    public void drawBall(Ball ball,Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = ball.getBallFace(); //shape of ball
+
+        g2d.setColor(ball.getInnerColor());
+        g2d.fill(s); //fill ball colour
+
+        g2d.setColor(ball.getBorderColor());
+        g2d.draw(s); //draw ball outline
+
+        g2d.setColor(tmp);
+    }
+
+    /**
+     * Draws the player's rectangle
+     * @param p The player object
+     * @param g2d An object which draws 2D components
+     */
+    public void drawPlayer(Player p,Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = p.getPlayerFace(); //shape of player
+        g2d.setColor(Player.INNER_COLOR);
+        g2d.fill(s); //fill player colour
+
+        g2d.setColor(Player.BORDER_COLOR);
+        g2d.draw(s); //draw player outline
+
+        g2d.setColor(tmp);
+    }
+
+    /**
+     * Sets up a tint and draws the pause menu
+     * @param g2d An object which draws 2D components
+     */
+    public void drawMenu(Graphics2D g2d){
+        obscureGameBoard(g2d);
+        drawPauseMenu(g2d);
+    }
+
+    /**
+     * Applies a tint over the game board when the pause menu is in effect
+     * @param g2d An object which draws 2D components
+     */
+    private void obscureGameBoard(Graphics2D g2d){
+        Composite tmp = g2d.getComposite();
+        Color tmpColor = g2d.getColor();
+
+        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.55f); //darkened effect during pause menu
+        g2d.setComposite(ac);
+
+        g2d.setColor(Color.BLACK); //colour of effect
+        g2d.fillRect(0,0,gameBoard.DEF_WIDTH,gameBoard.DEF_HEIGHT);
+
+        g2d.setComposite(tmp);
+        g2d.setColor(tmpColor);
+    }
+
+    /**
+     * Draws the pause menu
+     * Change:
+     * <ul>
+     *     <li>Moved code to create the button to drawPauseMenuButton</li>
+     *     <li>"this" was replaced with gameBoard</li>
+     *     <li>Used getters and setters for strLen</li>
+     * </ul>
+     * @param g2d An object which draws 2D components
+     */
+    private void drawPauseMenu(Graphics2D g2d){
+        Font tmpFont = g2d.getFont();
+        Color tmpColor = g2d.getColor();
+
+        g2d.setFont(menuFont);
+        g2d.setColor(MENU_COLOR); //set font colour
+
+        if(gameBoard.getStrLen() == 0){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            gameBoard.setStrLen(menuFont.getStringBounds(PAUSE,frc).getBounds().width); //gets width of word PAUSE with that font
+        }
+
+        int x = (gameBoard.getWidth() - gameBoard.getStrLen()) / 2; //center title
+        int y = gameBoard.getHeight() / 10;
+
+        g2d.drawString(PAUSE,x,y); //put PAUSE MENU
+
+        y = gameBoard.getHeight() / 4; //bottom of first quarter
+
+        continueButtonRect=drawPauseMenuButton(g2d, y, continueButtonRect, CONTINUE);
+
+        y *= 2; //second quarter
+
+        restartButtonRect=drawPauseMenuButton(g2d, y, restartButtonRect, RESTART);
+
+        y *= 3.0/2; //third quarter
+
+        exitButtonRect=drawPauseMenuButton(g2d, y, exitButtonRect, EXIT);
+
+        g2d.setFont(tmpFont);
+        g2d.setColor(tmpColor);
+    }
+
+    /**
+     * New Method - Moved code to create the buttons in pause menu from drawPauseMenu
+     * Change:
+     * <ul>
+     *     <li>"this" was changed with gameBoard</li>
+     * </ul>
+     * @param g2d An object which draws 2D components
+     * @param y The y-coordinate of the top left of the text
+     * @param rectangle The rectangle of the button
+     * @param text The string in the button
+     * @return The rectangle of the button
+     */
+    private Rectangle drawPauseMenuButton(Graphics2D g2d, int y, Rectangle rectangle, String text){
+        int x = gameBoard.getWidth() / 8; //margin for 3 options
+        if (rectangle==null){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            rectangle = menuFont.getStringBounds(CONTINUE,frc).getBounds(); //get width of word CONTINUE
+            rectangle.setLocation(x,y-rectangle.height); //put the button - button sits on the line of quarter
+        }
+        g2d.drawString(text,x,y); //put text
+        return rectangle;
+    }
+
+    /**
+     * New Method - Gets the CONTINUE button
+     * @return The rectangle of the button
+     */
+    public Rectangle getContinueButtonRect() {
+        return continueButtonRect;
+    }
+
+    /**
+     * New Method - Gets the EXIT button
+     * @return The rectangle of the button
+     */
+    public Rectangle getExitButtonRect() {
+        return exitButtonRect;
+    }
+
+    /**
+     * New Method - Gets the RESTART button
+     * @return The rectangle of the button
+     */
+    public Rectangle getRestartButtonRect() {
+        return restartButtonRect;
+    }
+}
